302e0e33c0f736b3635305c1bbfb759f1f2a3369
nebojsa_jacovic <nebojsa.jacovic@alterset.net>
2023-12-06 16:45:35 +0100

    Fix total pages data

--- internal/scraper/scraper.go
+++ internal/scraper/scraper.go
@@ -7,7 +7,6 @@ import (
 	"strconv"
 	"strings"
 	"sync"
-	"time"
 
 	"github.com/gocolly/colly/v2"
 )
@@ -26,20 +25,35 @@ func ScrapeMovies() ([]Movie, error) {
 	var wg sync.WaitGroup
 	var mu sync.Mutex
 	var totalPages int
-	var totalPagesMutex sync.Mutex
 
 	listCollector := colly.NewCollector()
-	movieCollector := colly.NewCollector()
+	pageCollector := colly.NewCollector()
 
-	listCollector.OnHTML(".movie-item-style-2", func(e *colly.HTMLElement) {
-		wg.Add(1)
-		go func(wg *sync.WaitGroup) {
-			defer wg.Done()
 
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		listCollector.OnHTML(".pagination__right", func(e *colly.HTMLElement) {
+			totalPagesStr := e.Text
+			re := regexp.MustCompile(`Page 1 of (\d+)`)
+			matches := re.FindStringSubmatch(totalPagesStr)
+		
+			var err error
+		
+			if len(matches) >= 2 {
+				totalPages, err = strconv.Atoi(matches[1])
+				if err != nil {
+					log.Printf("Error converting total pages: %v\n", err)
+					return
+				}
+			}
+		})
+
+		listCollector.OnHTML(".movie-item-style-2", func(e *colly.HTMLElement) {
 			title := e.ChildText(".mv-item-infor h6 a")
 			rate := e.ChildText(".rate span")
 			year := e.ChildText(".year")
-
 			link := e.ChildAttr("a[href]", "href")
 
 			movie := Movie{
@@ -48,69 +62,52 @@ func ScrapeMovies() ([]Movie, error) {
 				Year: year,
 			}
 
-			err := movieCollector.Visit(e.Request.AbsoluteURL(link))
+			overviewCollector := colly.NewCollector()
+
+			overviewCollector.OnHTML("#overview", func(e *colly.HTMLElement) {
+				genres := extractGenres(e)
+				duration := e.ChildText(".movie-description__duration span")
+				description := e.ChildText(".description")
+		
+				mu.Lock()
+				defer mu.Unlock()
+		
+				movie.Genres = genres
+				movie.Duration = duration
+				movie.Description = description
+
+				movies = append(movies, movie)
+			})
+
+			err := overviewCollector.Visit(e.Request.AbsoluteURL(link))
 			if err != nil {
 				log.Printf("Error visiting page %s: %v\n", link, err)
 				return
-			}
+			}	
+		})
 
-			movies = append(movies, movie)
-		}(&wg)		
-	})
+		listCollector.OnError(func(r *colly.Response, err error) {
+			log.Printf("Request URL: %s failed with response: %v\n", r.Request.URL, r)
+		})
 
-	movieCollector.OnHTML("#overview", func(e *colly.HTMLElement) {
-		genres := extractGenres(e)
-		duration := e.ChildText(".movie-description__duration span")
-		description := e.ChildText(".description")
-
-		mu.Lock()
-		defer mu.Unlock()
-
-		lastMovieIndex := len(movies) - 1
-		if lastMovieIndex >= 0 {
-			movies[lastMovieIndex].Genres = genres
-			movies[lastMovieIndex].Duration = duration
-			movies[lastMovieIndex].Description = description
-		}
-	})
-
-	listCollector.OnHTML(".pagination__right", func(e *colly.HTMLElement) {
-		totalPagesMutex.Lock()
-    	defer totalPagesMutex.Unlock()
-
-		totalPagesStr := e.Text
-		re := regexp.MustCompile(`Page 1 of (\d+)`)
-		matches := re.FindStringSubmatch(totalPagesStr)
-	
-		var err error
-	
-		if len(matches) >= 2 {
-			totalPages, err = strconv.Atoi(matches[1])
-			if err != nil {
-				log.Printf("Error converting total pages: %v\n", err)
-				return
-			}
-			log.Printf("Total Pages: %d\n", totalPages)
-		}
-	})
-
-	listCollector.OnError(func(r *colly.Response, err error) {
-		log.Printf("Request URL: %s failed with response: %v\n", r.Request.URL, r)
-	})
-
-	movieCollector.OnError(func(r *colly.Response, err error) {
-		log.Printf("Request URL: %s failed with response: %v\n", r.Request.URL, r)
-	})
-
-	log.Printf("Total Pages: %d\n", totalPages)
-	startUrl := "https://www.lookmovie2.to/page/%d?&r=5"
-	for i := 1; i <= 1; i++ {
-		err := listCollector.Visit(fmt.Sprintf(startUrl, i))
+		err := listCollector.Visit("https://www.lookmovie2.to/page/1?&r=5")
 		if err != nil {
-			log.Printf("Error visiting page %d: %v\n", i, err)
+			log.Printf("Error visiting page 1: %v\n", err)
 		}
+	}()
 
-		time.Sleep(1 * time.Second)
+	wg.Wait()
+	log.Printf("Total Pages: %d\n", totalPages)
+	for i := 1; i <= 1; i++ {
+		wg.Add(1)
+		go func(page int) {
+			defer wg.Done()
+
+			err := pageCollector.Visit(fmt.Sprintf("https://www.lookmovie2.to/page/%d?&r=5", page))
+			if err != nil {
+				log.Printf("Error visiting page %d: %v\n", page, err)
+			}
+		}(i)
 	}
 
 	wg.Wait()
